831ff530400f15963f6b47bba148debfc8e2bdc6
Dave Cowart <davecowart@gmail.com>
2012-10-17 10:50:13 -0500

    added Git-CompareBranchesMerged from Frank

--- shell/git.ps1
+++ shell/git.ps1
@@ -63,3 +63,27 @@ function github {
 	}
 	start $url
 }
+
+function Git-CompareBranchesMerged {
+  if ($args.Count -ne 2) {
+    "Usage: Git-CompareBranchesMerged branch1 branch2"
+    "This will list branches merged into branch1 which are not in branch2"
+    return
+  }
+  "Listing branches in $($args[0]) which are not in $($args[1])"
+  $merged1 = git branch -a --merged $args[0] | foreach { $_ -replace "remotes/origin/" -replace " " } | sort | unique
+  $merged2 = git branch -a --merged $args[1] | foreach { $_ -replace "remotes/origin/" -replace " " } | sort | unique
+  $currentColor = $Host.UI.RawUI.ForegroundColor
+  $Host.UI.RawUI.ForegroundColor = [System.ConsoleColor]"Red"
+  $branch1 = $args[0]
+  $merged1 | foreach {
+    $line1 = $_
+    $found = [Boolean]($merged2 | where { $_ -eq $line1 })
+    if (-not $found -and $line1 -replace " " -ne $branch1) {
+      $line1 = $line1 -replace "remotes/origin/"
+      "->  $line1"
+    }
+  }
+  $Host.UI.RawUI.ForegroundColor = $currentColor
+}
+Set-Alias gcbm Git-CompareBranchesMerged
