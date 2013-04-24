30384e92fc63e0b72bf782058db2ad1fcf61ea0d
Dave Cowart <davecowart@gmail.com>
2013-04-24 14:12:50 -0500

    added touch

--- shell/files.ps1
+++ shell/files.ps1
@@ -2,3 +2,7 @@ function killex {
 	param([string]$extension)
 	Get-ChildItem -Rec | Where {$_.Extension -match "$extension"} | Remove-Item
 }
+
+function touch {
+	set-content -Path ($args[0]) -Value ($null)
+}
